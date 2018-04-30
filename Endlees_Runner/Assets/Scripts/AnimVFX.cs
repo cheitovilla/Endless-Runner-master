11ebf633492509db6b6fa5f3f0099b8b0f8aa2c3
Cheitovilla <cheitovilla@outlook.com>
2018-04-30 16:57:09 -0500

    fbx_jump

---
+++ Endlees_Runner/Assets/Scripts/AnimVFX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimVFX : MonoBehaviour {
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (Input.GetKeyDown(KeyCode.X)) {
+			anim.SetTrigger ("jump");
+			gameObject.transform.position = new Vector3 (0.022f, 0f, gameObject.transform.position.z);
+		} 
+
+
+	}
+}
